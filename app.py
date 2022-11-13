5f68abfb3cb895df91bbef0c855f96d6451c7e20
rvitality <vital.renzjohn@gmail.com>
2022-11-13 17:05:12 +0800

    add new routes

--- app.py
+++ app.py
@@ -7,9 +7,23 @@ final = ""
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    
     return render_template("index.html")
 
 
+@app.route("/piano", methods=["GET", "POST"])
+def piano():
+    return render_template("piano.html")
+
+
+@app.route("/authentication", methods=["GET", "POST"])
+def authentication():
+    return render_template("authentication.html")
+
+
+@app.route("/speech_to_text", methods=["GET", "POST"])
+def speech_to_text():
+    return render_template("speech_to_text.html")
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
